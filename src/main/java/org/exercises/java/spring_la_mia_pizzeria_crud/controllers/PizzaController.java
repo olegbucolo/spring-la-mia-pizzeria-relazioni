3a283fdbce4a2478f0d9cd90d61b47027065aaea
olegbucolo <olegbucolo97@gmail.com>
2026-04-09 00:14:07 +0200

    END ex spring-la-mia-pizzeria-crud step 1

--- src/main/java/org/exercises/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
+++ src/main/java/org/exercises/java/spring_la_mia_pizzeria_crud/controllers/PizzaController.java
@@ -5,7 +5,9 @@ import org.exercises.java.spring_la_mia_pizzeria_crud.repositories.PizzaReposito
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -30,5 +32,17 @@ public class PizzaController {
         .orElseThrow(() -> new RuntimeException("Pizza no found"));
         model.addAttribute("pizza", pizza);
         return "pizza-detail";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model){
+        model.addAttribute("pizza", new Pizza());
+        return "pizza-create";
+    }
+
+    @PostMapping
+    public String store(@ModelAttribute Pizza pizza){
+        pr.save(pizza);
+        return "redirect:/pizzas";
     }
 }
